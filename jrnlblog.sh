db048a9d8607c1c281690a2dac8bd15534c27616
nicoloren <nicolaslorenzon@yahoo.fr>
2016-12-09 15:05:48 +0100

    Add config file config.sh

--- jrnlblog.sh
+++ jrnlblog.sh
@@ -17,28 +17,8 @@ if [ -z "$1" ]; then
 fi
 TAG=$1
 
-# Some configuration ----------------
-# You should modify these
-BLOG_NAME="My Journal"
-TEMPLATE_FILE="templates/template.html"
-MD_EXTENSION=".markdown"
-HTML_EXTENSION=".html"
-TITLE_TAG="!TITLE!"
-DATE_TAG="!DATE!"
-CONTENT_TAG="!CONTENT!"
-USE_DATE_AS_FILENAME=true
-EXPORT_DIR="export" # without "/" at the end
-# END of configuration --------------
-
-
-# TAG specific configuration --------
-# you can add tag specific config
-if [ "$TAG" = "work" ] ; then
-    BLOG_NAME="Hello world !"
-    TEMPLATE_FILE="templates/pure.html"
-    USE_DATE_AS_FILENAME=false
-fi
-# END of configuration---------------
+# include config
+source config.sh
 
 # Color definitions
 RED='\033[0;31m'
